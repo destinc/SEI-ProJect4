52ad4f0868e22d753d161b5942b8bdfd91d00ecc
Destin Coleman <destinc@Destins-MacBook-Air.local>
2019-09-11 23:45:57 -0400

    rails seeded and rendering

---
+++ ideaboard-api/db/seeds.rb
@@ -0,0 +1,27 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+ideas = Idea.create(
+[
+
+{
+    title: "New App",
+    body: "Awesome App"
+},
+
+{
+    title: "Card Game Design",
+    body: "Cross between Whist and Pokemon"
+},
+
+{
+    title: "Twitter Clone",
+    body: "Like twitter but for food"
+},
+
+])
